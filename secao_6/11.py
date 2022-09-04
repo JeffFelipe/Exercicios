0ccc214a09977ea273134037d7f50219418ed11d
JFelipe <jeffersonfelipe.nm@hotmail.com>
2022-09-04 14:22:19 -0300

    initial Commit

--- secao_6/11.py
+++ secao_6/11.py
@@ -1,6 +1,7 @@
 print("Digite um número inteiro positivo")
 
 n = int(input())
-lista = list(range(0, n, 1))
+if n > -1:
+    lista = list(range(0, n, 1))
 
-print(f"Os números naturais de 0 a {n} são:\n{lista}")
+    print(f"Os números naturais de 0 a {n} são:\n{lista}")

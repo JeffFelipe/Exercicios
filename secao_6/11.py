72e6c5b0b05262a0387233e8e278b1740b6b23fc
JFelipe <jeffersonfelipe.nm@hotmail.com>
2022-09-04 14:52:33 -0300

    initial Commit

--- secao_6/11.py
+++ secao_6/11.py
@@ -2,6 +2,6 @@ print("Digite um número inteiro positivo")
 
 n = int(input())
 if n > -1:
-    lista = list(range(0, n, 1))
+    lista = list(range(0, (n + 1), 1))
 
     print(f"Os números naturais de 0 a {n} são:\n{lista}")

b02ee3b9b6ca09b1b95557f9d4fa273367a20f46
JFelipe <jeffersonfelipe.nm@hotmail.com>
2022-09-04 13:36:06 -0300

    initial Commit

---
+++ secao_6/9.py
@@ -0,0 +1,35 @@
+print("Digite um número inteiro qualquer.\n")
+
+ni = []
+
+adiciona = int(input())
+
+if adiciona % 2 != 0:
+    while len(ni) < 10:
+        ni.append(adiciona + 2)
+        adiciona = adiciona + 2
+else:
+    while len(ni) < 10:
+        ni.append(adiciona + 1)
+        adiciona = adiciona + 1
+
+print(f"Os próximos 10 números naturais ímpares são:\n{ni}")
+
+"""
+print("Digite um número inteiro qualquer.\n")
+
+ni = []  # criei uma lista sem nenhum valor
+
+adiciona = int(input())  # campo para usuário inserir o número
+
+if adiciona % 2 != 0:  # Se a variável adiciona dividida por 2 e tiver como resto diferente de 0
+    while len(ni) < 10:  # Enquanto o len da lista ni for menor que 10 índice
+        ni.append(adiciona + 2)  # irá inserir na lista ni o valor inicial de adiciona + 2
+        adiciona = adiciona + 2  # modifica o valor de adiciona, crianod o looping
+else:  # Se não
+    while len(ni) < 10:
+        ni.append(adiciona + 1)
+        adiciona = adiciona + 1
+
+print(f"Os próximos 10 números naturais ímpares são:\n{ni}")
+"""

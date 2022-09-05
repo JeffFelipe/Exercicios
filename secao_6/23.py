cd3aaa87d66a6ec07c31342d8f7ea416a8090e92
JFelipe <jeffersonfelipe.nm@hotmail.com>
2022-09-05 15:39:39 -0300

    initial Commit

---
+++ secao_6/23.py
@@ -0,0 +1,16 @@
+print("Digite um número positivo")
+
+num = int(input())
+
+i = 1
+
+divisores = []
+
+if num < 0:
+    num = int(input('Digite um número positivo: '))
+
+for i in range(i, num+1):
+    if num % i == 0:
+        divisores.append(i)
+
+print(f"Os divisores de {num} são {divisores[::-1]}")
